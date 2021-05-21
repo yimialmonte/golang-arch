831ff86460b8ef3d33e8c2f4f7ae2b9f68dd7192
yimialmonte <yelmialmonte@gmail.com>
2021-05-21 14:48:59 -0400

    json decode and encode example

--- main.go
+++ main.go
@@ -2,38 +2,65 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
+	"net/http"
 )
 
 type person struct {
 	First string `json:"first"`
 }
 
-func main() {
-	p1 := person{
-		First: "Yelmi",
-	}
+const port = "127.0.0.1:8080"
 
-	p2 := person{
+func main() {
+	// p1 := person{
+	// 	First: "Yelmi",
+	// }
+
+	// p2 := person{
+	// 	First: "Juan",
+	// }
+
+	// xp := []person{p1, p2}
+
+	// bs, err := json.Marshal(xp)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+
+	// fmt.Println("Json Format", string(bs))
+
+	// xp2 := []person{}
+
+	// err = json.Unmarshal(bs, &xp2)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+
+	// fmt.Println("Back to Go structure", xp2)
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", bar)
+
+	http.ListenAndServe(port, nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	p1 := person{
 		First: "Juan",
 	}
 
-	xp := []person{p1, p2}
-
-	bs, err := json.Marshal(xp)
+	err := json.NewEncoder(w).Encode(p1)
 	if err != nil {
-		log.Panic(err)
+		log.Println("Enocde error: ", err)
+	}
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	var p1 person
+	err := json.NewDecoder(r.Body).Decode(&p1)
+	if err != nil {
+		log.Println("Decode data", err)
 	}
 
-	fmt.Println("Json Format", string(bs))
-
-	xp2 := []person{}
-
-	err = json.Unmarshal(bs, &xp2)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	fmt.Println("Back to Go structure", xp2)
+	log.Println(p1)
 }
